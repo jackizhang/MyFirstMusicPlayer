9e03a880c9046fac10eb1c1ecfd5fd193e3252d7
zhangyunpu <yunp.zhang@gmail.com>
2017-04-21 18:01:45 +0800

    commit second time , save for home work

--- app/src/main/java/com/jackyzhang/myfirstmusicplayer/home/adapter/FragmentAdapter.java
+++ app/src/main/java/com/jackyzhang/myfirstmusicplayer/home/adapter/FragmentAdapter.java
@@ -1,5 +1,7 @@
 package com.jackyzhang.myfirstmusicplayer.home.adapter;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 /**
@@ -7,4 +9,17 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  */
 
 public class FragmentAdapter extends FragmentStatePagerAdapter {
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
